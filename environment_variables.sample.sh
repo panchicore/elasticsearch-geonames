ee046cd51a1b4938f045ee286a07b878c1ec24e1
Luis Pallares <luis@pallares.co>
2018-09-14 10:55:40 +0200

    add more env vars

--- environment_variables.sample.sh
+++ environment_variables.sample.sh
@@ -6,6 +6,14 @@ export ES_GEONAMES_PASSWORD=
 export ES_GEONAMES_INDEX=geonames
 
 # dont modify this if you download the files in the default location:
+export ES_GEONAMES_CACHE_FEATURECODES=$(pwd)\/tmp\/dict_featurecodes.csv
+export ES_GEONAMES_CACHE_ISOCODES=$(pwd)\/tmp\/dict_country_isocode.csv
+export ES_GEONAMES_CACHE_COUNTRYID=$(pwd)\/tmp\/dict_country_geonameid.csv
+export ES_GEONAMES_CACHE_ADMIN1FEATURE=$(pwd)\/tmp\/dict_admin1_feature.csv
+export ES_GEONAMES_CACHE_ADMIN2FEATURE=$(pwd)\/tmp\/dict_admin2_feature.csv
+export ES_GEONAMES_CACHE_ADMIN1ID=$(pwd)\/tmp\/dict_admin1_geonameid.csv
+export ES_GEONAMES_CACHE_ADMIN2ID=$(pwd)\/tmp\/dict_admin2_geonameid.csv
+
 export ES_GEONAMES_FILE=$(pwd)\/tmp\/allCountries.txt
 export ES_GEONAMES_BOUNDINGBOXES_FILE=$(pwd)\/tmp\/boundingbox.txt
 export ES_GEONAMES_POLYGONS_FILE=$(pwd)\/tmp\/allshapes.txt
